c5c7a851e71f44ecfe71990555731076806af37b
Anna Archive <ytpt@mail.ru>
2024-01-07 03:35:25 -0500

    Added loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Nunito_Sans } from 'next/font/google'
 import './globals.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const nunitoSans = Nunito_Sans({subsets: ['latin']})
 
@@ -16,10 +17,10 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
             <body className={nunitoSans.className}>
                 <main className="bg-gray-100 min-h-screen flex flex-col items-center">
                     <nav className="w-full bg-white h-16 flex items-center justify-center">
-                        <section className="container flex items-center gap-3">
+                        <Link href="/" className="container flex items-center gap-3">
                             <Image src="/logo.jpg" alt="Earth" width={48} height={48}/>
                             <h1 className="font-bold text-2xl">Countries App</h1>
-                        </section>
+                        </Link>
                     </nav>
                     {children}
                 </main>
